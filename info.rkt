53c1f9ac0eeed7a757252a1c4a6427937f0e0ac1
HaeckGabriel <53453199+HaeckGabriel@users.noreply.github.com>
2022-11-04 14:36:45 -0400

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,9 +1,14 @@
 #lang info
 
 (define name "Regression")
+
+;; version
+(define version "1.0")
+
+;; dependencies
 (define deps '("base"
                "math-lib"
                "csv-reading"))
-               
+;; Documentation               
 (define scribblings
   '(("scribblings/linear-regression.scrbl" ())))
