3b9718fc854593da466571be9b80ae19eac6a470
HaeckGabriel <53453199+HaeckGabriel@users.noreply.github.com>
2022-11-16 18:27:26 -0500

    Update regression.rkt

--- example/regression.rkt
+++ example/regression.rkt
@@ -7,3 +7,5 @@
 (coeffs x_y_mat) ;; calculate the coefficients
 
 (r2_adj (coeffs x_y_mat) x_y_mat) ;; calculate adjusted R^2
+
+(predict (coeffs x_y_mat) (vector 1 2)) ;; calculate a prediction. New x values must be a vector.
