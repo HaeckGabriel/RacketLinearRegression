76f55a255194803a0f9be3c4cea1c9abe2b80bf4
HaeckGabriel <53453199+HaeckGabriel@users.noreply.github.com>
2022-11-16 18:26:11 -0500

    Update regression.rkt

--- example/regression.rkt
+++ example/regression.rkt
@@ -2,8 +2,8 @@
 
 (require linear-regression)
 
-(define x_y_mat (csv-transform "data.csv"))
+(define x_y_mat (csv-transform "data.csv")) ;; create the x and y matrices
 
-(coeffs x_y_mat)
+(coeffs x_y_mat) ;; calculate the coefficients
 
-(r2_adj (coeffs x_y_mat) x_y_mat)
+(r2_adj (coeffs x_y_mat) x_y_mat) ;; calculate adjusted R^2
